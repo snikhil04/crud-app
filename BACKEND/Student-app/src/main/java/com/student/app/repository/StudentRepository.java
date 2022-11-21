fd9928564aae057873b4f731ad02ba38e5ecc3e6
Nikhil Kumar Singh <nikhil.kumar@iot83.com>
2022-11-21 21:51:37 +0530

    .

--- BACKEND/Student-app/src/main/java/com/student/app/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.student.app.repository;
-
-import com.student.app.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student,String> {
-}
